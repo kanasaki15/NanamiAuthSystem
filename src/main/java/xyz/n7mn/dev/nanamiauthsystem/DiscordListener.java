9bba1522de0ee35a639dc1d2de81ddb31f965acf
tina-chan <kassi_0123@kassi-hp.info>
2022-01-24 23:49:36 +0900

    BEアドレス変更対応

--- src/main/java/xyz/n7mn/dev/nanamiauthsystem/DiscordListener.java
+++ src/main/java/xyz/n7mn/dev/nanamiauthsystem/DiscordListener.java
@@ -216,7 +216,8 @@ public class DiscordListener extends ListenerAdapter {
                 // チャンネル・ロール作成
                 String mapName = "auth"+event.getAuthor().getId();
                 builder.setDescription("" +
-                        "1.Minecraftで「`"+plugin.getConfig().getString("LobbyIP")+" (BEは「`"+plugin.getConfig().getString("LobbyBEIP")+"`」)`」に入る\n" +
+                        "1.Minecraft Javaでは「`"+plugin.getConfig().getString("LobbyIP")+"`」" +
+                        "  Minecraft Bedrockでは「`"+plugin.getConfig().getString("LobbyBEIP")+"`」に入る\n" +
                         "2.以下の文字列をMinecraftのチャットで入力をする。\n" +
                         "\n" +
                         "`vy."+token+"`\n" +
